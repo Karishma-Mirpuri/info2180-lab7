4af2a665956a7493ffe40ed6981ecb9d622ae2e5
Karishma Mirpuri <karishma.mirpuri@mymona.uwi.edu>
2016-11-15 04:04:57 +0000

    Exercise 4

--- world.js
+++ world.js
@@ -1,9 +1,23 @@
 window.onload = function() {
     var searching = document.getElementById("lookup");
     searching.addEventListener("click", function() {
+        
         var httpRequest = new XMLHttpRequest();
+        var all_countries = '';
         var search_country = document.getElementById("country").value;
-            httpRequest.open('GET', 'world.php?country=' + search_country);
+        var check = document.getElementById("checkbox");
+        
+        if (search_country !== '' ||  check.checked)
+        {    
+            if (check.checked)
+            {
+                all_countries = check.value;
+            }
+            else
+            {
+                all_countries = '';
+            }
+            httpRequest.open('GET', 'world.php?country=' + search_country + '&all=' + all_countries);
             httpRequest.onload = function() {
                 if (httpRequest.status === 200) {
                     //alert('httpRequest.responseText');
@@ -13,7 +27,11 @@ window.onload = function() {
                     alert('There was a problem with the request.');
                 }    
             }; 
-
-    httpRequest.send();
+            httpRequest.send();
+        }
+        else
+        {
+            document.getElementById('result').innerHTML = 'Please enter a valid country to lookup, or select the checkbox to lookup all countries.';
+        }
     });
 };
--- world.php
+++ world.php
@@ -1,6 +1,7 @@
 <?php
 
 $query = $_GET['country'];
+$query2 = $_GET['all'];
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
@@ -8,14 +9,27 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-
-
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$query%'");
+$stmt2 = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+if ($query2 == 'true')
+{
+    $results = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 }
-echo '</ul>';
+else
+{
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+if (count($results) == 0)
+{
+    echo 'Please enter a valid country to lookup, or select the checkbox to lookup all countries.';
+}
+else
+{
+    echo '<ul>';
+    foreach ($results as $row) {
+        echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
